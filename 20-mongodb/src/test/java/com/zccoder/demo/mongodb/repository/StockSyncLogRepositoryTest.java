28cc725348c413336179b6f7cdd9fe2e9398c7ef
zccodere <zccoder@aliyun.com>
2020-05-09 14:18:30 +0800

    新增mongodb入门学习源码

--- 20-mongodb/src/test/java/com/zccoder/demo/mongodb/repository/StockSyncLogRepositoryTest.java
+++ 20-mongodb/src/test/java/com/zccoder/demo/mongodb/repository/StockSyncLogRepositoryTest.java
@@ -9,11 +9,19 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
 
 /**
  * 库存同步日志数据层测试类
@@ -24,13 +32,15 @@ import java.util.Optional;
 public class StockSyncLogRepositoryTest extends MongodbApplicationTest {
 
     @Autowired
+    private MongoTemplate mongoTemplate;
+    @Autowired
     private StockSyncLogRepository stockSyncLogRepository;
 
     @Test
     public void save() {
         StockSyncLog log = new StockSyncLog();
         log.setGoodsId(10);
-        log.setStockNum(7);
+        log.setStockNum(3);
         log.setSyncCode(0);
         log.setSyncMessage("成功");
         log.setCreatetime(LocalDateTime.now());
@@ -48,7 +58,7 @@ public class StockSyncLogRepositoryTest extends MongodbApplicationTest {
     @Test
     public void last() {
         StockSyncLog log = new StockSyncLog();
-        log.setGoodsId(10);
+        log.setGoodsId(11);
         Example<StockSyncLog> example = Example.of(log);
 
         Sort sort = Sort.by(Sort.Direction.DESC, "createtime");
@@ -57,5 +67,27 @@ public class StockSyncLogRepositoryTest extends MongodbApplicationTest {
 
         List<StockSyncLog> content = page.getContent();
         content.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectIn() {
+        // https://docs.spring.io/spring-data/mongodb/docs/2.2.7.RELEASE/reference/html/#mongodb-template-query
+        Query query = Query.query(Criteria.where("goodsId").in(10, 11));
+        // 类似SQL：select * from table where goodsId in (10,11)
+        List<StockSyncLog> logList = mongoTemplate.find(query, StockSyncLog.class);
+        logList.forEach(System.out::println);
+    }
+
+    @Test
+    public void groupBy() {
+
+        Criteria goodsId = Criteria.where("goodsId").in(10, 11, 12);
+        Criteria syncCode = Criteria.where("syncCode").is(1);
+
+        Aggregation aggregation = Aggregation.newAggregation(new MatchOperation(goodsId), new MatchOperation(syncCode), group("goodsId").last("stockNum").as("stockNum").last("goodsId").as("goodsId"));
+
+        AggregationResults<StockSyncLog> stockSyncLog = mongoTemplate.aggregate(aggregation, "stockSyncLog", StockSyncLog.class);
+        List<StockSyncLog> logList = stockSyncLog.getMappedResults();
+        logList.forEach(System.out::println);
     }
 }
